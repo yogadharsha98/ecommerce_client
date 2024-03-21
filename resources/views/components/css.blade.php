ebf49d437393faa3e4e29aab8cdebc20037fa100
yogakrishanthi98 <yogakrishanthi@konnect.team>
2024-03-21 15:43:26 +0530

    changed logo name

--- resources/views/components/css.blade.php
+++ resources/views/components/css.blade.php
@@ -1,5 +1,5 @@
 <meta charset="utf-8">
-<title>Fruitables - Vegetable Website Template</title>
+<title>Samson's</title>
 <meta content="width=device-width, initial-scale=1.0" name="viewport">
 <meta content="" name="keywords">
 <meta content="" name="description">
